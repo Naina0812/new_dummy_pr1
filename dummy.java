97e99aaa19a6567e98861b48adbbffce936b3df9
naincy0812 <naincy.chourasia@apphelix.ai>
2025-05-03 00:03:12 +0530

    Create dummy.java

---
+++ dummy.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    
+    // Function name: printHelloWorld
+    public static void printHelloWorld() {
+        System.out.println("Hello, World");
+    }
+
+    public static void main(String[] args) {
+        printHelloWorld(); // Calling the function
+    }
+}
