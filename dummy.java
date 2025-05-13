8bb536a5d5d323643436b1dab1b6c88ca5fc910d
Naincy Chourasia <106032516+Naina0812@users.noreply.github.com>
2025-05-13 12:15:42 +0530

    Update dummy.java

--- dummy.java
+++ dummy.java
@@ -1,35 +1,9 @@
-import java.util.ArrayList;
+public class Main {
+    public static void main(String args) {
+        int num = "10"; // Error 1: assigning String to int
+        System.out.println("The number is: " + num;
 
-public class StudentGrader {
-
-    public static void main(String[] args) {
-        ArrayList<String> studentNames = new ArrayList<>();
-        studentNames.add("Alice");
-        studentNames.add("Bob");
-        studentNames.add("Charlie");
-
-        ArrayList<Integer> grades = new ArrayList<>();
-        grades.add(85);
-        grades.add(92);
-        grades.add("78"); // Error: adding a string to an Integer list
-
-        for (int i = 0; i <= studentNames.size(); i++) { // Error: index out of bounds
-            String name = studentNames.get(i);
-            int grade = grades.get(i);
-
-            printResult(name, grade); // Error: method not static
-        }
-
-        System.out.println("Grading complete!")
-    }
-
-    public void printResult(String name, int grade) { // Error: method not static
-        if (grade > 90) {
-            System.out.println(name + " received an A.");
-        } else if (grade >= 80) {
-            System.out.println(name + " received a B.");
-        else { // Error: missing closing brace for else if
-            System.out.println(name + " needs improvement.");
-        }
+        if(num > 5) // Error 2: Missing braces, and num is a String
+            System.out.println("Number is greater than 5");
     }
 }
